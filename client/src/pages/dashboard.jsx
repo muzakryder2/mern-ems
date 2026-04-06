3bf32d2486ee0530f6546487769341bdf080ec09
muzakryder2 <dcastilloafm2011@gmail.com>
2026-04-06 18:19:38 -0500

    Added Employee Dashboard header

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import { dummyEmployeeDashboardData } from "../assets/assets";
+import Loading from "../components/loading";
+import EmployeeDashboard from "../components/employee-dashboad";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setData(dummyEmployeeDashboardData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!data) {
+    return (
+      <p className="text-center text-slate-500 py-12">
+        Failed to load dasboard
+      </p>
+    );
+  }
+
+  if (data.role === "ADMIN") {
+    return <div className="">admin dashboard</div>;
+  } else {
+    return <EmployeeDashboard data={data} />;
+  }
 }
--- client/src/pages/layout.jsx
+++ client/src/pages/layout.jsx
@@ -10,7 +10,6 @@ export default function Layout() {
           <Outlet />
         </div>
       </main>
-      <Outlet />
     </div>
   );
 }
